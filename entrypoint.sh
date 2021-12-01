46ab3181a126011f7be7d4fe4010b88a7ea8cab0
Jesse Newland <jesse@jnewland.com>
2021-12-01 11:33:58 -0600

    dynamic port

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,16 @@ export CASED_SHELL_TLS=off
 let SSH_PORT=PORT+1 ;
 export CASED_SHELL_OAUTH_UPSTREAM=localhost:$SSH_PORT
 
+echo "starting ssh server"
 PORT=$SSH_PORT /bin/heroku-ssh heroku http://localhost:$PORT bash -i &
+
+echo "updating port in prompts.json"
+mv prompts.json prompts.pre.json
+jq --arg placeholder SSH_PORT --arg port $SSH_PORT \
+  '.prompts | map((select(.port == $placeholder) | .port) |= $port) | { prompts: .}' \
+    prompts.pre.json > prompts.json
+
+echo "starting cased shell server"
 python -u run.py --logging=$CASED_SHELL_LOG_LEVEL &
 ps axjf
 wait -n
