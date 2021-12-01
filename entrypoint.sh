bafa79ff4b4edbb78b985a2b63cf54cdb12063b8
Jesse Newland <jesse@jnewland.com>
2021-12-01 11:48:35 -0600

    correct upstream

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ let SSH_PORT=PORT+1 ;
 export CASED_SHELL_OAUTH_UPSTREAM=localhost:$SSH_PORT
 
 echo "starting ssh server"
-PORT=$SSH_PORT /bin/heroku-ssh heroku http://localhost:$PORT bash -i &
+PORT=$SSH_PORT /bin/heroku-ssh heroku https://$CASED_SHELL_HOSTNAME bash -i &
 
 echo "updating port in prompts.json"
 jq --arg placeholder SSH_PORT --arg port $SSH_PORT \
